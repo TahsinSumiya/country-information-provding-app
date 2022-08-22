a931e6f339a6bbef30a5107a571d527ab3030a8b
TahsinSumiya <tahsinadity187@gmail.com>
2022-08-22 23:14:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
 import './App.css';
-
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 function App() {
+  const url = "https://restcountries.com/v3.1/all";
+  const [isLoading,setIsLoading] = useState(true);
+  const [error,setError]=useState(null);
+  const [countries,setCountries]=useState([]);
+
+  const fetchData = async (url) =>{
+    setIsLoading(true);
+
+ 
+
+   try {
+    const response = await fetch(url);
+    const data = await response.json();
+    setCountries(data)
+    setIsLoading(false)
+    setError(null)
+    console.log(countries)
+
+   } catch (error) {
+    setIsLoading(false)
+    setError(error)
+   }
+  }
+
+  useEffect (()=>{
+    fetchData(url)
+    },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+   <>
+   <h1>Country app</h1>
+   {isLoading && <h1>{AiOutlineLoading3Quarters}</h1>}
+   {error && <h2>{error}</h2>}
+   </>
+   
   );
 }
 
