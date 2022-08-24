54038de49a3c85211805667c0919443ba477e6e0
TahsinSumiya <tahsinadity187@gmail.com>
2022-08-24 22:44:19 +0600

    useEffect & search,remove

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { useState,useEffect } from 'react';
 import './App.css';
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import Countries from './component/Countries';
+import Search from './component/Search'
 function App() {
   const url = "https://restcountries.com/v3.1/all";
   const [isLoading,setIsLoading] = useState(true);
   const [error,setError]=useState(null);
   const [countries,setCountries]=useState([]);
+  
 
   const fetchData = async (url) =>{
     setIsLoading(true);
-
- 
 
    try {
     const response = await fetch(url);
@@ -30,12 +31,29 @@ function App() {
     fetchData(url)
     },[])
 
+    const handleRemoveCountry=(name)=>{
+      const filter = countries.filter((country)=>country.name.common !== name);
+      setCountries(filter)
+
+    }
+
+    const handleSearch=(searchhValue)=>{
+      let value=searchhValue.toLowerCase()
+      const newCountries = countries.filter((country)=>{
+        const countryName = country.name.common.toLowerCase()
+        return countryName.startsWith(value)
+      })
+      setCountries(newCountries)
+    }
+
   return (
     
    <>
-   <h1>Country app</h1>
-   {isLoading && <h1>{AiOutlineLoading3Quarters}</h1>}
-   {error && <h2>{error}</h2>}
+  <h1>Country app</h1>
+  <Search onSearch={handleSearch}/>
+   {isLoading && <h1><AiOutlineLoading3Quarters/></h1>}
+   {error && <h2>{error.message}</h2>}
+   {countries && <Countries countries={countries} onRemoveCountry={handleRemoveCountry}/> }
    </>
    
   );
---
+++ src/component/Search.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+
+const Search = (props) => {
+    const [searchText,setSearchText]=useState("")
+    
+    const handleChange=(e)=>{
+        setSearchText(e.target.value)
+        
+    }
+useEffect(()=>{
+    props.onSearch(searchText)
+},[searchText])
+
+  return (
+    <div style={{textAlign:"center"}}>
+      <input type="text" placeholder="Searched Country"
+      value={searchText}
+      onChange={handleChange}
+      ></input>
+    </div>
+  )
+}
+
+export default Search
